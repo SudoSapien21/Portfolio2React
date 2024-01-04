8f8cdfd8bb4aee92ecf5d72c7deb21a23a75d1bb
Clarence Tolson <sudosapien21@gmail.com>
2024-01-04 17:05:17 -0600

    updated home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,9 +6,9 @@ function Home() {
     <div className="home">
       <h2>Home Page</h2>
       
-      <p>Welcome to my portfolio; as you navigate the file, you will understand who I AM and whose I AM. As time passes,
-         I will continue to add more content to this site, giving more in-depth information about me and the projects I have been working on. "...We ask ourselves, who am I to be brilliant, gorgeous, talented, fabulous? Actually, 
-         who are you not to be? You are a child of God. Your playing small does not serve the world. There is nothing enlightened about shrinking so that other people won't feel insecure around you..." Nelson Mandela</p>
+      <p>Greetings and welcome to my portfolio. As you explore this space, you'll gain insights into both my identity and the impactful projects I've been involved in. Over time, I'll be enriching this site with additional content, providing a more comprehensive view of my journey and accomplishments.
+
+Reflecting on the words of Nelson Mandela, "We ask ourselves, who am I to be brilliant, gorgeous, talented, fabulous? Actually, who are you not to be? You are a child of God. Your playing small does not serve the world. There is nothing enlightened about shrinking so that other people won't feel insecure around you."</p>
 
          
           
